792e2dc23a336cc758d363f9482618f94de38eaf
M4deN <alexdesaran@outlook.com>
2023-09-27 11:14:35 -0300

    config cypress cloud and refactor package.json and cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,15 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 9000,
+  requestTimeout: 9000,
+  video: true,
   e2e: {
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // },
     baseUrl: 'https://notes-serverless-app.com',
     env: {
       viewportWidthBreakpoint: 768,
     },
-    chromeWebSecurity: false,
-    defaultCommandTimeout: 9000,
-    requestTimeout: 9000,
-    video: true,
   },
+  projectId: '7kxmh2',
 })

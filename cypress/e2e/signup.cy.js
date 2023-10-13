4ecd82d2261b65117c415ac6c48cbd07fdd66ad3
M4deN <alexdesaran@outlook.com>
2023-10-13 13:46:08 -0300

    Update signup.cy.js

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -6,7 +6,7 @@ describe('Sign up', () => {
   const emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
   const password = Cypress.env('USER_PASSWORD')
 
-  it('successfully signs up using confirmation code sent via email', () => {
+  it.skip('successfully signs up using confirmation code sent via email', () => {
     cy.fillSignupFormAndSubmit(emailAddress, password)
 
     cy.contains('h1', 'Your Notes').should('be.visible')
